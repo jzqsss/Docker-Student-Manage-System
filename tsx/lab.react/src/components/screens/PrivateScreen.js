47af67df78563cf33748bed6a49d0ddcaeb91265
jzqsss <295974773@qq.com>
2022-04-27 13:16:33 +0800

    成功把表建好了

--- tsx/lab.react/src/components/screens/PrivateScreen.js
+++ tsx/lab.react/src/components/screens/PrivateScreen.js
@@ -2,6 +2,55 @@ import { useState, useEffect } from "react";
 import { useHistory} from "react-router-dom";
 import axios from "axios";
 import "./PrivateScreen.css";
+import user from "../forms/containers";
+import { Table, Tag, Space } from "antd";
+import 'antd/dist/antd.css';
+
+const columns = [
+  { title: '镜像序号', dataIndex: 'name', key: 'name' },
+  { title: '镜像名称', dataIndex: 'age', key: 'age' },
+  { title: 'URL （image）', dataIndex: 'address', key: 'address' },
+  {
+    title: '备注',
+    dataIndex: '',
+    key: 'x',
+    render: () => <a>创建实例</a>,
+  },
+];
+
+const data = [
+  {
+    key: 1,
+    name: 'John Brown',
+    age: 32,
+    address: 'New York No. 1 Lake Park',
+    description: 'My name is John Brown, I am 32 years old, living in New York No. 1 Lake Park.',
+  },
+  {
+    key: 2,
+    name: 'Jim Green',
+    age: 42,
+    address: 'London No. 1 Lake Park',
+    description: 'My name is Jim Green, I am 42 years old, living in London No. 1 Lake Park.',
+  },
+  {
+    key: 3,
+    name: 'Not Expandable',
+    age: 29,
+    address: 'Jiangsu No. 1 Lake Park',
+    description: 'This not expandable',
+  },
+  {
+    key: 4,
+    name: 'Joe Black',
+    age: 32,
+    address: 'Sidney No. 1 Lake Park',
+    description: 'My name is Joe Black, I am 32 years old, living in Sidney No. 1 Lake Park.',
+  },
+];
+
+
+
 
 const PrivateScreen = () => {
   const [error, setError] = useState("");
@@ -37,11 +86,22 @@ const PrivateScreen = () => {
   return error ? (
     <span className="error-message">{error}</span>
   ) : (
-
-    <div style={{ background:'green', color:'white' }}>
-      {privateData}
-      <button onClick={handleLogout}>logout</button>
-    </div>
+    
+        
+    <Table
+    columns={columns}
+    expandable={{
+      expandedRowRender: record => <p style={{ margin: 0 }}>{record.description}</p>,
+      rowExpandable: record => record.name !== 'Not Expandable',
+    }}
+    dataSource={data}
+  />
+      
+    // <div style={{ background:'green', color:'white' }}>
+    //   {privateData}
+      
+    //   <button onClick={handleLogout}>logout</button>
+    // </div>
   );
 };
 
